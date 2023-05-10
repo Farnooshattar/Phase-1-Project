ddce5e6c65027be84bfdd440648b2ee55dc025fc
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-05-10 10:32:28 -0400

    gets the elements from the html file

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+   //gets the elements from the html file
+   const pic = document.getElementById("picture");
+   const startBtn = document.querySelector("#start_btn");
+   const revealBoard = document.querySelector(".reveals");
+   const countdownElement = document.getElementById("countdown");
 }) 
