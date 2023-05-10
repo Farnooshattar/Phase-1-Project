fc466b88e365d0b86c1400fac9ad3cc9a15673be
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-05-10 10:35:46 -0400

    get the audio file for the mouseover events and intialize the variables

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
    const startBtn = document.querySelector("#start_btn");
    const revealBoard = document.querySelector(".reveals");
    const countdownElement = document.getElementById("countdown");
+   //get the audio file for the mouseover events
+   const g3 = new Audio("g3-04.wav");
+   //intialize the variables
+   let reveals = 0;
+   let timeUp = false;
+   let timeLeft = 0;
+   let countdownInterval;
 }) 
